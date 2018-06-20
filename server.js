33a81fc2493f25241d9b36e22736c2c73150766d
Luke Hardin <lhardin491@gmail.com>
2018-06-20 11:00:25 -0500

    Actually fixed chrome issue
    My mistake in the last commit, didn't realize I actually forgot to make the change. Changed this in the server.js file to allow cross origin access.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', null);
+    res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, app-id');
     if (req.method === "OPTIONS")
